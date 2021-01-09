3cfdba340b79ed51f052947638e15febaa14ea59
Shuai_Yan <1018718155@qq.com>
2021-01-09 20:32:59 +0800

    new robot

--- press_gun/robot.py
+++ press_gun/robot.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 import threading
-from pynput import keyboard, mouse
+
 from PyQt5.QtCore import pyqtSignal, QObject
+from pynput import keyboard, mouse
 
 from image_detect.Detector import Detector
-from state.position_constant import get_pos, pos_names
-from state.all_states import All_States
-from screen_capture import win32_cap
-
 from press_gun.press import Press
+from screen_capture import win32_cap
+from state.all_states import All_States
+from state.position_constant import get_pos, pos_names
 
 
 class Temp_QObject(QObject):
@@ -51,9 +51,9 @@ class Robot:
     def on_press(self, key):
         if key == keyboard.Key.tab:
             self.screen = get_screen()
-            threading.Timer(0.5, self.is_in_tab).start()
+            threading.Timer(0.01, self.is_in_tab).start()
         if key == keyboard.Key.f12:
-            self.all_states.dont_press = True
+            self.all_states.to_press = False
 
         if key == keyboard.Key.ctrl_l and self.calibrate:
             self.ctrl_save_screen()
@@ -61,17 +61,18 @@ class Robot:
         if hasattr(key, 'char'):
             key = key.char
         if key == 'g' or key == '5':
-            self.all_states.dont_press = True
+            self.all_states.to_press = False
         if key == 'b':
-            self.all_states.dont_press = False
-            threading.Timer(0.5, self.set_fire_mode).start()
+            self.all_states.to_press = True
+            self.set_fire_mode()
         if key == '1' or key == '2':
-            self.all_states.dont_press = False
+            self.all_states.to_press = True
             self.all_states.set_weapon_n(int(key) - 1)
-            threading.Timer(0.5, self.set_fire_mode).start()
+            self.set_fire_mode()
 
     def on_click(self, x, y, button, pressed):
-        if button == mouse.Button.left and pressed and (not self.all_states.dont_press):
+        if button == mouse.Button.left and pressed and self.all_states.to_press:
+        # if button == mouse.Button.left and pressed:
             n = self.all_states.weapon_n
             if self.all_states.weapon[n].is_press:
                 self.press = Press(self.all_states.weapon[n].dist_seq, self.all_states.weapon[n].time_seq,
@@ -82,9 +83,9 @@ class Robot:
             if hasattr(self, 'press'):
                 self.press.stop()
 
-        if button in [mouse.Button.right, mouse.Button.left] and (not pressed):
-            if self.all_states.screen_state == 'tab':
-                threading.Timer(0.2, self.tab_func).start()
+        # if button in [mouse.Button.right, mouse.Button.left] and (not pressed):
+        #     if self.all_states.screen_state == 'tab':
+        #         threading.Timer(0.2, self.tab_func).start()
 
     def ctrl_save_screen(self):
         n = self.all_states.weapon_n
@@ -96,14 +97,15 @@ class Robot:
         win32_cap(os.path.join(save_dir, str(i) + ".png"))
 
     def is_in_tab(self):
-        if 'in_tab' == self.in_tab_detect.im2name(crop_screen(self.screen, 'in_tab')):
+        go_out_of_tab = 'in_tab' == self.in_tab_detect.im2name(crop_screen(self.screen, 'in_tab'))
+        if go_out_of_tab:
+            self.all_states.to_press = False
             self.all_states.screen_state = 'tab'
-            self.all_states.dont_press = True
+            self.tab_func()
         else:
-            if self.all_states.screen_state == 'tab':
-                threading.Timer(0.5, self.set_fire_mode).start()
+            threading.Timer(0.2, self.set_fire_mode).start()
             self.all_states.screen_state = 'default'
-            self.all_states.dont_press = False
+            self.all_states.to_press = True
 
     def tab_func(self):
         for pos_name in pos_names:
@@ -120,19 +122,19 @@ class Robot:
         self.print_state()
 
     def set_fire_mode(self):
-        fire_mode_crop = get_screen('fire_mode')
-        fire_mode = self.fire_mode_detect.im2name(fire_mode_crop)
+        # fire_mode_crop = get_screen('fire_mode')
+        # fire_mode = self.fire_mode_detect.im2name(fire_mode_crop)
         n = self.all_states.weapon_n
-        self.all_states.weapon[n].set('fire_mode', fire_mode)
+        self.all_states.weapon[n].set('fire_mode', "")
         self.print_state()
 
     def print_state(self):
         w = self.all_states.weapon[0]
         gun1_state = str(w.name) + '-' + str(w.fire_mode) + '-' + str(w.scope) + '-' + str(w.muzzle)[:3] + '-' + str(
-            w.grip)
+            w.grip) + '-' + str(w.butt)
         w = self.all_states.weapon[1]
         gun2_state = str(w.name) + '-' + str(w.fire_mode) + '-' + str(w.scope) + '-' + str(w.muzzle)[:3] + '-' + str(
-            w.grip)
+            w.grip) + '-' + str(w.butt)
         if self.all_states.weapon_n == 0:
             emit_str = ' * ' + gun1_state + '\n' + gun2_state
         else:
