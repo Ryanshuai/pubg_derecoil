aeb0901fe524923012fab536509528791e5a6eea
Ryan <1018718155@qq.com>
2022-09-05 13:26:06 -0700

    update crop position

--- state/position_constant.py
+++ state/position_constant.py
@@ -1,12 +1,8 @@
 # [y1, x1, y2, x2]
 
 def get_pos(name):
-    name_tuple = crop_position[name]
-    if len(name_tuple) == 2:
-        y, x = name_tuple
-        return y, x, 64, 64
-    y1, x1, y2, x2 = name_tuple
-    return y1, x1, y2 - y1, x2 - x1
+    yxhw = crop_position[name]
+    return yxhw
 
 
 pos_names = ['gun1_name', 'gun1_scope', 'gun1_muzzle', 'gun1_grip', 'gun1_butt',
@@ -14,23 +10,23 @@ pos_names = ['gun1_name', 'gun1_scope', 'gun1_muzzle', 'gun1_grip', 'gun1_butt',
 
 crop_position = {
     'fire_mode': [1330, 1648, 1364, 1676],
-    'in_tab': [198, 747, 231, 815],
+    'in_tab': [198, 747, 33, 68],
     'posture': [1005, 705, 1048, 743],
     # 'in_scope': [1669, 1179, 1766, 1208],
 
-    'gun1_name': [190, 2740, 195 + 60, 2742 + 110],
-    'gun1_scope': [152, 2554],
-    'gun1_muzzle': [330, 2191],
-    'gun1_grip': [330, 2327],
-    # 'gun1_magazine': [330, 2474],
-    'gun1_butt': [330, 2756],
+    'gun1_name': [190, 2740, 60, 110],
+    'gun1_scope': [229, 3204, 100, 100],
+    'gun1_muzzle': [494, 2657, 100, 100],
+    'gun1_grip': [494, 2863, 100, 100],
+    # 'gun1_magazine': [330, 2474, 100, 100],
+    'gun1_butt': [494, 3508, 100, 100],
 
-    'gun2_name': [436, 2250, 436 + 35, 2250 + 64],
-    'gun2_scope': [458, 2554],
-    'gun2_muzzle': [636, 2191],
-    'gun2_grip': [636, 2327],
-    # 'gun2_magazine': [636, 2474],
-    'gun2_butt': [636, 2756]
+    'gun2_name': [652, 2740, 60, 110],
+    'gun2_scope': [690, 3203, 100, 100],
+    'gun2_muzzle': [952, 2658, 100, 100],
+    'gun2_grip': [952, 2864, 100, 100],
+    # 'gun2_magazine': [636, 2474, 100, 100],
+    'gun2_butt': [955, 3508, 100, 100],
 }
 
 screen_position_states = \
