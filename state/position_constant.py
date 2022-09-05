28943d378396674be9afbde6655139fd8fa6f973
Ryan <1018718155@qq.com>
2022-09-05 15:55:03 -0700

    for 1080p screen and 4k screen

--- state/position_constant.py
+++ state/position_constant.py
@@ -1,14 +1,9 @@
-# [y1, x1, y2, x2]
+from screeninfo import get_monitors
 
-def get_pos(name):
-    yxhw = crop_position[name]
-    return yxhw
+monitors = get_monitors()
+main_monitor = [monitor for monitor in monitors if monitor.is_primary][0]
 
-
-pos_names = ['gun1_name', 'gun1_scope', 'gun1_muzzle', 'gun1_grip', 'gun1_butt',
-             'gun2_name', 'gun2_scope', 'gun2_muzzle', 'gun2_grip', 'gun2_butt']
-
-crop_position = {
+crop_position_3480_2160 = {
     'fire_mode': [1330, 1648, 1364, 1676],
     'in_tab': [198, 747, 33, 68],
     'posture': [1005, 705, 1048, 743],
@@ -29,42 +24,15 @@ crop_position = {
     'gun2_butt': [955, 3508, 100, 100],
 }
 
-screen_position_states = \
-    {'fire_mode': ['single', 'burst2', 'burst3', 'full'],
-     'in_tab': ['in'],
-     'in_scope': ['in'],
-     'weapon1name': ['98k', 'm24', 'awm', 'mini14', 'mk14', 'qbu', 'sks', 'slr', 'vss', 'akm', 'aug', 'groza', 'm416',
-                     'qbz', 'scar', 'm762', 'g36c', 'm16', 'mk47', 'tommy', 'uzi', 'ump45', 'vector', 'pp19', 'm249',
-                     'dp28', 's12k', 's1987', 's686', 'win94', ],
-     'weapon2name': ['98k', 'm24', 'awm', 'mini14', 'mk14', 'qbu', 'sks', 'slr', 'vss', 'akm', 'aug', 'groza', 'm416',
-                     'qbz', 'scar', 'm762', 'g36c', 'm16', 'mk47', 'tommy', 'uzi', 'ump45', 'vector', 'pp19', 'm249',
-                     'dp28', 's12k', 's1987', 's686', 'win94', ],
-     'weapon1scope': ['1r', '1h', '2', '3', '4', '6', '8', '15'],
-     'weapon2scope': ['1r', '1h', '2', '3', '4', '6', '8', '15'],
-     'weapon1muzzle': ['ar_flash', 'ar_suppressor', 'ar_compensator', 'smg_flash', 'smg_suppressor', 'smg_compensator',
-                       'sp_flash', 'sp_suppressor', 'sp_compensator'],
-     'weapon2muzzle': ['ar_flash', 'ar_suppressor', 'ar_compensator', 'smg_flash', 'smg_suppressor', 'smg_compensator',
-                       'sp_flash', 'sp_suppressor', 'sp_compensator'],
-     'weapon1grip': ['thumb', 'lightweight', 'half', 'angled', 'vertical'],
-     'weapon2grip': ['thumb', 'lightweight', 'half', 'angled', 'vertical'],
-     'weapon1butt': ['m416_butt'],
-     'weapon2butt': ['m416_butt'],
-     'weapon1magazine': [],
-     'weapon2magazine': [],
-     'helmet': [],
-     'armor': [],
-     'backpack': [],
-     'ground0': [],
-     'ground1': [],
-     'ground2': [],
-     'ground3': [],
-     'ground4': [],
-     'ground5': [],
-     'ground6': [],
-     'ground7': [],
-     'ground8': [],
-     'ground9': [],
-     'ground10': [],
-     'ground11': [],
-     'ground12': []
-     }
+if main_monitor.width == 3480 and main_monitor.height == 2160:
+    crop_position = crop_position_3480_2160
+
+if main_monitor.width == 1920 and main_monitor.height == 1080:
+    crop_position = crop_position_3480_2160
+    for k, v in crop_position.items():
+        crop_position[k] = [int(x * 0.5) for x in v]
+
+
+def get_pos(name):
+    yxhw = crop_position[name]
+    return yxhw
